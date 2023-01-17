63a420b20c81151cf851d508d2fceef51746d641
Anshuman <anshuman8swain@gmail.com>
2023-01-17 21:14:13 +0530

    fix:typescript strict made

--- components/Form/AntibioticUsed.tsx
+++ components/Form/AntibioticUsed.tsx
@@ -1,147 +1,127 @@
-import React, {} from 'react'
-import { GrAddCircle } from 'react-icons/gr'
+import React from "react";
+import { GrAddCircle } from "react-icons/gr";
 
 export default function AntibioticUsed() {
+  const [antibiotic, setAntibiotic] = React.useState([{}]);
+  const [antibioticCount, setAntibioticCount] = React.useState(1);
 
-    const [antibiotic, setAntibiotic] = React.useState([{}])
-    const [antibioticCount, setAntibioticCount] = React.useState(1)
-
-    const addAntibiotic = () => {
-        setAntibioticCount(antibioticCount + 1)
-        setAntibiotic([...antibiotic, antibioticCount])
-    }
+  const addAntibiotic = () => {
+    setAntibioticCount(antibioticCount + 1);
+    setAntibiotic([...antibiotic, antibioticCount]);
+  };
 
   return (
     <div>
-      <div className="text-lg text-white font-semibold mt-2 my-1 flex flex-row">
-            <div>
-            Antibiotic Used{" "}
-            </div>
-            <div className='border-2 border-amber-400 p-1 mx-1 rounded-md hover:cursor-pointer' onClick={addAntibiotic}>
-                <GrAddCircle className='fill-amber-500' />
-            </div>
+      <div className="text-lg text-white font-semibold mt-2 mb-2 my-1 flex flex-row">
+        <div>Antibiotic Used </div>
+        <div
+          className="border-2 border-amber-400 p-1 mx-1 rounded-md hover:cursor-pointer"
+          onClick={addAntibiotic}
+        >
+          <GrAddCircle className="fill-amber-500" />
+        </div>
+      </div>
+      {antibiotic.map((d, i) => (
+        <div className="flex flex-wrap mb-0 mx-1" key={i}>
+          <div className="w-full md:w-[33%] mb-4 md:mb-0">
+            <label className="label-upper" htmlFor="email">
+              Antibiotic:
+            </label>
+            <input
+              required
+              className="input-imp"
+              name=""
+              id=""
+              type="text"
+              placeholder=""
+            />
           </div>
-          {antibiotic.map((d,i)=>(
-            <div className="flex flex-wrap mb-5 mx-1" key={i}>
-            <div className="w-full md:w-[33%] mb-6 md:mb-0">
-              <label
-                className="label-upper"
-                htmlFor="email"
-              >
-                Antibiotic:
-              </label>
-              <input
-                required
-                className="input-imp"
-                name=""
-                id=""
-                type="text"
-                placeholder=""
-              />
-            </div>
 
-            <div className="w-full  md:w-[11.12%] px-2 mb-6 md:mb-0">
-              <label
-                className="label-upper"
-                htmlFor="email"
-              >
-                Dose:
-              </label>
-              <input
-                required
-                className="input-imp"
-                name=""
-                id=""
-                type="text"
-                placeholder=""
-              />
-            </div>
+          <div className="w-full  md:w-[11.12%] px-2 mb-4 md:mb-0">
+            <label className="label-upper" htmlFor="email">
+              Dose:
+            </label>
+            <input
+              required
+              className="input-imp"
+              name=""
+              id=""
+              type="text"
+              placeholder=""
+            />
+          </div>
 
-            <div className="w-full  md:w-[11.12%] px-2 mb-6 md:mb-0">
-              <label
-                className="label-upper"
-                htmlFor="email"
-              >
-                 Route:
-              </label>
-              <input
-                required
-                className="input-imp"
-                name=""
-                id=""
-                type="text"
-                placeholder=""
-              />
-            </div>
+          <div className="w-full  md:w-[11.12%] px-2 mb-4 md:mb-0">
+            <label className="label-upper" htmlFor="email">
+              Route:
+            </label>
+            <input
+              required
+              className="input-imp"
+              name=""
+              id=""
+              type="text"
+              placeholder=""
+            />
+          </div>
 
-            <div className="w-full  md:w-[11.12%] px-2 mb-6 md:mb-0">
-              <label
-                className="label-upper"
-                htmlFor="email"
-              >
-                Frequency:
-              </label>
-              <input
-                required
-                className="input-imp"
-                name=""
-                id=""
-                type="text"
-                placeholder=""
-              />
-            </div>
+          <div className="w-full  md:w-[11.12%] px-2 mb-4 md:mb-0">
+            <label className="label-upper" htmlFor="email">
+              Frequency:
+            </label>
+            <input
+              required
+              className="input-imp"
+              name=""
+              id=""
+              type="text"
+              placeholder=""
+            />
+          </div>
 
-            <div className="w-full  md:w-[11.12%] px-2 mb-6 md:mb-0">
-              <label
-                className="label-upper"
-                htmlFor="email"
-              >
-                init. Date:
-              </label>
-              <input
-                required
-                className="input-imp"
-                name=""
-                id=""
-                type="text"
-                placeholder=""
-              />
-            </div>
+          <div className="w-full  md:w-[11.12%] px-2 mb-4 md:mb-0">
+            <label className="label-upper" htmlFor="email">
+              init. Date:
+            </label>
+            <input
+              required
+              className="input-imp"
+              name=""
+              id=""
+              type="text"
+              placeholder=""
+            />
+          </div>
 
-            <div className="w-full  md:w-[11.12%] px-2 mb-6 md:mb-0">
-              <label
-                className="label-upper"
-                htmlFor="email"
-              >
-                Loading Dose:
-              </label>
-              <input
-                required
-                className="input-imp"
-                name=""
-                id=""
-                type="text"
-                placeholder=""
-              />
-            </div>
+          <div className="w-full  md:w-[11.12%] px-2 mb-4 md:mb-0">
+            <label className="label-upper" htmlFor="email">
+              Loading Dose:
+            </label>
+            <input
+              required
+              className="input-imp"
+              name=""
+              id=""
+              type="text"
+              placeholder=""
+            />
+          </div>
 
-            <div className="w-full md:w-[11.12%] md:pl-3 md:pr-1 mb-6 md:mb-0">
-              <label
-                className="label-upper"
-                htmlFor="email"
-              >
-                Infusion:
-              </label>
-              <input
-                required
-                className="input-imp"
-                name=""
-                id=""
-                type="text"
-                placeholder=""
-              />
-            </div>
-          </div>))}
+          <div className="w-full md:w-[11.12%] md:pl-3 md:pr-1 mb-4 md:mb-0">
+            <label className="label-upper" htmlFor="email">
+              Infusion:
+            </label>
+            <input
+              required
+              className="input-imp"
+              name=""
+              id=""
+              type="text"
+              placeholder=""
+            />
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
--- pages/form.tsx
+++ pages/form.tsx
@@ -6,10 +6,14 @@ import Link from "next/link";
 import CultureReport from "../components/Form/CultureReport";
 import AntibioticUsed from "../components/Form/AntibioticUsed";
 
+interface CultureReportType {
+  report: number;
+}
+
 export default function Form() {
   const [loading, setLoading] = useState(false);
   const [cultureSent, setCultureSent] = useState(false);
-  const [cultureReportList, setCultureReportList] = useState([]);
+  const [cultureReportList, setCultureReportList] = useState<CultureReportType[] | []>([]);
 
   const notify = (message: String) => toast.error(message);
 
@@ -27,96 +31,98 @@ export default function Form() {
   };
 
   const addCultureReport = () => {
-    if(cultureSent){
-      const n = cultureReportList[cultureReportList.length-1].report  + 1 
-      setCultureReportList(cultureReportList.concat({"report": n}));
-    }else{
+    if (cultureSent) {
+      const n = cultureReportList[cultureReportList.length - 1].report + 1;
+      setCultureReportList([...cultureReportList, { report: n }]);
+    } else {
       setCultureSent(true);
-      setCultureReportList(cultureReportList.concat({"report": 1}));
+      setCultureReportList([...cultureReportList, { report: 1 }]);
     }
   };
 
   const deleteCultureReport = (report: number) => {
-    if(cultureSent){
-      setCultureReportList(cultureReportList.filter((item) => item.report !== report));
+    if (cultureSent) {
+      setCultureReportList(
+        cultureReportList.filter((item) => item.report !== report)
+      );
     }
-    if(cultureReportList.length<=1){
+    if (cultureReportList.length <= 1) {
       setCultureSent(false);
     }
-  }
+  };
 
-  const clinicalSigns=[{
-    name:"Temp(F)",
-    id:"cstempf",
-    placeholder:""
-  },
-  {
-    name:"Blood Pressure(mmHg)",
-    id:"csbp",
-    placeholder:"BP"
-  },
-  {
-    name:"O2 Saturation (%)",
-    id:"csosat",
-    placeholder:""
-  },
-  {
-    name:"White Blood Cells (K/uL)",
-    id:"cswbc",
-    placeholder:""
-  },
-  {
-    name:"CRP (mg/L)",
-    id:"cscrp",
-    placeholder:""
-  },
-  {
-    name:"Procalcitonin (ng/ml)",
-    id:"csprocalc",
-    placeholder:""
-  },
-  {
-    name:"Lactate (mmol/L)",
-    id:"cslactate",
-    placeholder:""
-  },
-  {
-    name:"S.Creatinine(mg/dl)",
-    id:"cscreatinine",
-    placeholder:""
-  },
-  {
-    name:"Neutrophils %",
-    id:"csneutrophils",
-    placeholder:""
-  },
-  {
-    name:"Platelets (109/L)",
-    id:"csplatelets",
-    placeholder:""
-  },
-  {
-    name:"Urine analysis",
-    id:"csurine",
-    placeholder:""
-  },
-  {
-    name:"CSF study",
-    id:"csfstudy",
-    placeholder:""
-  },
-  {
-    name:"Body fluid study",
-    id:"csbodyfluid",
-    placeholder:""
-  },
-  // {
-  //   name:"",
-  //   id:"",
-  //   placeholder:""
-  // },
-]
-
+  const clinicalSigns = [
+    {
+      name: "Temp(F)",
+      id: "cstempf",
+      placeholder: "",
+    },
+    {
+      name: "Blood Pressure(mmHg)",
+      id: "csbp",
+      placeholder: "BP",
+    },
+    {
+      name: "O2 Saturation (%)",
+      id: "csosat",
+      placeholder: "",
+    },
+    {
+      name: "White Blood Cells (K/uL)",
+      id: "cswbc",
+      placeholder: "",
+    },
+    {
+      name: "CRP (mg/L)",
+      id: "cscrp",
+      placeholder: "",
+    },
+    {
+      name: "Procalcitonin (ng/ml)",
+      id: "csprocalc",
+      placeholder: "",
+    },
+    {
+      name: "Lactate (mmol/L)",
+      id: "cslactate",
+      placeholder: "",
+    },
+    {
+      name: "S.Creatinine(mg/dl)",
+      id: "cscreatinine",
+      placeholder: "",
+    },
+    {
+      name: "Neutrophils %",
+      id: "csneutrophils",
+      placeholder: "",
+    },
+    {
+      name: "Platelets (109/L)",
+      id: "csplatelets",
+      placeholder: "",
+    },
+    {
+      name: "Urine analysis",
+      id: "csurine",
+      placeholder: "",
+    },
+    {
+      name: "CSF study",
+      id: "csfstudy",
+      placeholder: "",
+    },
+    {
+      name: "Body fluid study",
+      id: "csbodyfluid",
+      placeholder: "",
+    },
+    // {
+    //   name:"",
+    //   id:"",
+    //   placeholder:""
+    // },
+  ];
 
   return (
     <div className="bg-secondary h-screen w-full relative p-2">
@@ -157,10 +163,7 @@ export default function Form() {
           </div>
 
           <div className="w-full md:w-1/3 mb-6 md:mb-5">
-            <label
-              className="label-upper"
-              htmlFor=""
-            >
+            <label className="label-upper" htmlFor="">
               Reviewing Department
             </label>
             <input
@@ -179,10 +182,7 @@ export default function Form() {
           </div>
           <div className="flex flex-wrap mb-5 mx-1">
             <div className="w-full md:w-1/3 mb-6 md:mb-0">
-              <label
-                className="label-upper"
-                htmlFor="email"
-              >
+              <label className="label-upper" htmlFor="email">
                 Provisional diagnosis:
               </label>
               <input
@@ -196,10 +196,7 @@ export default function Form() {
             </div>
 
             <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label
-                className="label-upper"
-                htmlFor="email"
-              >
+              <label className="label-upper" htmlFor="email">
                 Final diagnosis:
               </label>
               <input
@@ -213,10 +210,7 @@ export default function Form() {
             </div>
 
             <div className="w-full md:w-3/12 md:pl-3 md:pr-1 mb-6 md:mb-0">
-              <label
-                className="label-upper"
-                htmlFor="email"
-              >
+              <label className="label-upper" htmlFor="email">
                 Syndromic diagnosis:
               </label>
               <input
@@ -261,7 +255,7 @@ export default function Form() {
             </div>
           </div>
 
-          {/* infection */}
+          {/* Suspected focus of infection*/}
           <div className="mb-5">
             <div className="text-white my-3 text-base font-semibold">
               Suspected Focus Of Infection
@@ -384,99 +378,98 @@ export default function Form() {
 
           {cultureSent && (
             <>
-              {cultureReportList.map((li,i)=> <CultureReport key={i} id={li.report} deleteCultureReport={deleteCultureReport}/>) }
+              {cultureReportList.map((li, i) => (
+                <CultureReport
+                  key={i}
+                  id={li.report }
+                  deleteCultureReport={deleteCultureReport}
+                />
+              ))}
             </>
           )}
 
-          {cultureSent && <div className="-mt-3 mx-3 mb-3 ">
-            <button
-              type="button"
-              onClick={()=>addCultureReport()}
-              className="bg-blue-500 text-white p-2 mb-2 font-medium rounded-md shadow-lg active:shadow-sm"
-            >
-              Add Another Report
-            </button>
-          </div>}
+          {cultureSent && (
+            <div className="-mt-3 mx-3 mb-3 ">
+              <button
+                type="button"
+                onClick={() => addCultureReport()}
+                className="bg-blue-500 text-white p-2 mb-2 font-medium rounded-md shadow-lg active:shadow-sm"
+              >
+                Add Another Report
+              </button>
+            </div>
+          )}
 
           {/* Antibiotic used*/}
           <>
-           <AntibioticUsed/>
+            <AntibioticUsed />
           </>
 
-           {/* Clinical Signs correlating with Antibiotic initiation(prior 48 hours) */}
-           <div className="text-lg text-white font-semibold mt-4 my-2">
-           Clinical Signs correlating with Antibiotic initiation(prior 48 hours){" "}
+          {/* Clinical Signs correlating with Antibiotic initiation(prior 48 hours) */}
+          <div className="text-lg text-white font-semibold mt-4 my-2">
+            Clinical Signs correlating with Antibiotic initiation(prior 48
+            hours){" "}
           </div>
           <div className="flex flex-wrap mb-5 mx-1">
-            {clinicalSigns.map((imp,i)=>
-            <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0" key={i}>
-              <label
-                className="block capitalize tracking-wide text-sm font-bold mb-2 text-white"
-                htmlFor="email"
-              >
-                {imp.name}
-              </label>
-              <input
-                required
-                className="appearance-none block w-full bg-gray-100 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none  focus:bg-white"
-                name="department"
-                id={imp.id}
-                type="text"
-                placeholder={imp.placeholder}
-              />
-            </div>)}
+            {clinicalSigns.map((imp, i) => (
+              <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0" key={i}>
+                <label
+                  className="block capitalize tracking-wide text-sm font-bold mb-2 text-white"
+                  htmlFor="email"
+                >
+                  {imp.name}
+                </label>
+                <input
+                  required
+                  className="appearance-none block w-full bg-gray-100 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none  focus:bg-white"
+                  name="department"
+                  id={imp.id}
+                  type="text"
+                  placeholder={imp.placeholder}
+                />
+              </div>
+            ))}
           </div>
-
 
           {/* Comments */}
           <div className="flex flex-wrap mb-5 mx-1">
+            <div className="w-full md:w-1/3 px-1 mb-6 md:mb-5">
+              <label className="label-upper" htmlFor="">
+                Classification of patient:
+              </label>
+              <input
+                required
+                className="input-imp"
+                name="department"
+                id="department"
+                placeholder=""
+              />
+            </div>
+            <div className="w-full md:w-1/3 px-1 mb-6 md:mb-5">
+              <label className="label-upper" htmlFor="">
+                Classification of disease/ diagnosis:
+              </label>
+              <input
+                required
+                className="input-imp"
+                name="department"
+                id="department"
+                placeholder=""
+              />
+            </div>
 
-          <div className="w-full md:w-1/3 px-1 mb-6 md:mb-5">
-            <label
-              className="label-upper"
-              htmlFor=""
-            >
-              Classification of patient:
-            </label>
-            <input
-              required
-              className="input-imp"
-              name="department"
-              id="department"
-              placeholder=""
-            />
-          </div>
-          <div className="w-full md:w-1/3 px-1 mb-6 md:mb-5">
-            <label
-              className="label-upper"
-              htmlFor=""
-            >
-              Classification of disease/ diagnosis:
-            </label>
-            <input
-              required
-              className="input-imp"
-              name="department"
-              id="department"
-              placeholder=""
-            />
-          </div>
-
-          <div className="w-full md:w-1/3 pr-1 mb-6 md:mb-5">
-            <label
-              className="label-upper"
-              htmlFor=""
-            >
-              Comments
-            </label>
-            <textarea
-              required
-              className="input-imp"
-              name="department"
-              id="department"
-              placeholder="Comments"
-            />
-          </div>
+            <div className="w-full md:w-1/3 pr-1 mb-6 md:mb-5">
+              <label className="label-upper" htmlFor="">
+                Comments
+              </label>
+              <textarea
+                required
+                className="input-imp"
+                name="department"
+                id="department"
+                placeholder="Comments"
+              />
+            </div>
           </div>
 
           {/* Submit */}
