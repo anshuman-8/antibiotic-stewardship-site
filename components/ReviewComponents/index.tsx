76a1423534911fba540156347d6891084f67a6bb
Anshuman Swain <anshuman8swain@gmail.com>
2023-06-06 21:09:29 +0530

    minor updates in analysis report

--- components/ReviewComponents/index.tsx
+++ components/ReviewComponents/index.tsx
@@ -4,31 +4,39 @@ import InfectionFocus from "./InfectionFocus";
 import AntibioticUsed from "./AntibioticUsed";
 import CultureReports from "./CultureReports";
 import ClinicalSigns from "./ClinicalSigns";
-import Carousel from "flowbite-react";
+// import Carousel from "flowbite-react";
 
 export default function index({ data }) {
-  console.log("for review,", data);
 
   return (
     <div className="analytics-card">
-      {/* <div>Report</div> */}
       <div className="text-xl text-white font-semibold mt-2 my-2">Report: </div>
       <Diagnosis data={data} />
       <InfectionFocus data={data} />
-      <AntibioticUsed data={data.antibioticsUsed} />
-      <div className="text-white font-medium mt-4 text-lg">
+      {data?.antibioticsUsed.length === 0?<AntibioticUsed data={data?.antibioticsUsed} />:<NoData />}
+      <div className="text-white font-medium mt-4 mb-4 text-lg">
         Culture Reports:
       </div>
-      {data.cultureReport.length===0?
-      <span className="px-5 py-2 bg-slate-400 mb-3 rounded-md w-min">None</span>
-      :<div className="flex flex-row overflow-auto mb-3 mt-1">
-        <div>
-          {data.cultureReport.map((report, i) => {
-            return <CultureReports key={i} id={i} report={report} />;
-          })}
+      {data.cultureReport.length === 0 ? (
+        <NoData/>
+      ) : (
+        <div className="flex flex-row overflow-auto mb-3 mt-1">
+          <div>
+            {data.cultureReport.map((report, i) => {
+              return <CultureReports key={i} id={i} report={report} />;
+            })}
+          </div>
         </div>
-      </div>}
-      <ClinicalSigns date={data.reviewDate} patient={data.patient.id} />
+      )}
+      {data?.reviewDate?<ClinicalSigns date={data?.reviewDate} patient={data.patient.id} />:<NoData />}
     </div>
   );
 }
+
+const NoData = () => {
+  return (
+    <div className="flex flex-col bg-slate-400 w-min rounded-md px-5 py-2 mb-3 -mt-3">
+      <div className="font-base text-lg">None</div>
+    </div>
+  );
+};
